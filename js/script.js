f9d6dd2d1d5c5cfe3eda7c3984d3069aea89d8b4
judegor99 <125523787+judegor99@users.noreply.github.com>
2023-10-27 18:42:58 -0500

    Add files via upload
    UPLOAD

--- js/script.js
+++ js/script.js
@@ -1,41 +1,44 @@
 document.addEventListener('DOMContentLoaded', function () {
-  const imageCarousel = document.getElementById('image-carousel');
-  const imageDescription = document.getElementById('image-description');
-  const fadeInDuration = 1000; // Duración de la animación de aparición (en milisegundos)
-  const fadeOutDuration = 1000; // Duración de la animación de desaparición (en milisegundos)
-  let currentIndex = 0;
-
-  function showImageWithDescription() {
-      const slides = imageCarousel.querySelectorAll('.image-slide');
-
+    const imageCarousel = document.getElementById('image-carousel');
+    const imageDescription = document.getElementById('image-description');
+    const prevButton = document.getElementById('prev-button');
+    const nextButton = document.getElementById('next-button');
+    const slides = imageCarousel.querySelectorAll('.image-slide');
+    let currentIndex = 0;
+  
+    function showImageWithDescription() {
       if (currentIndex >= slides.length) {
-          currentIndex = 0;
+        currentIndex = 0;
       }
-
+      // Mostrar la imagen actual
       slides.forEach((slide, i) => {
-          if (i === currentIndex) {
-              slide.style.display = 'block';
-          } else {
-              slide.style.display = 'none';
-          }
+        if (i === currentIndex) {
+          slide.style.display = 'block';
+        } else {
+          slide.style.display = 'none';
+        }
       });
-
+  
+      // Actualizar la descripción
       const currentSlide = slides[currentIndex];
       const description = currentSlide.querySelector('.carousel-description').textContent;
       imageDescription.textContent = description;
-
-      currentIndex++;
-
-      setTimeout(hideCurrentSlide, 5000); // Cambiar cada 5 segundos
-  }
-
-  function hideCurrentSlide() {
-      const currentSlide = imageCarousel.querySelector('.image-slide[style="display: block;"]');
-      if (currentSlide) {
-          currentSlide.style.display = 'none';
-      }
+    }
+  
+    function showPrevImage() {
+      currentIndex = (currentIndex - 1 + slides.length) % slides.length;
       showImageWithDescription();
-  }
-
-  showImageWithDescription();
-});
+    }
+  
+    function showNextImage() {
+      currentIndex = (currentIndex + 1) % slides.length;
+      showImageWithDescription();
+    }
+  
+    prevButton.addEventListener('click', showPrevImage);
+    nextButton.addEventListener('click', showNextImage);
+  
+    // Mostrar la primera imagen y descripción al cargar la página
+    showImageWithDescription();
+  });
+  
